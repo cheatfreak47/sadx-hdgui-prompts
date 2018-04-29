8eed228f3a7f96d47342cf2d3d0ba32a249457e8
CheatFreak <cheatfreak47@gmail.com>
2018-04-29 11:29:19 -0400

    couple refinements

--- Slightly Useful Batch Scripts/copydupes.bat
+++ Slightly Useful Batch Scripts/copydupes.bat
@@ -1,6 +1,6 @@
-cp TUTOMSG_SONIC_E_HD/sprf_01_e.png TUTOMSG_TAILS_E_HD/sprf_01_e.png
-cp TUTOMSG_SONIC_E_HD/sprf_02_e.png TUTOMSG_TAILS_E_HD/sprf_02_e.png
-cp TUTOMSG_SONIC_E_HD/sprf_04_e.png TUTOMSG_TAILS_E_HD/sprf_04_e.png
-cp TUTOMSG_TAILS_E_HD/tprf_06_e.png TUTOMSG_KNUCKLES_E_HD/tprf_06_e.png
-cp TUTOMSG_AMY_E_HD/aprf_04_e.png TUTOMSG_BIG_E_HD/aprf_04_e.png
-cp TUTOMSG_AMY_E_HD/aprf_04_e.png TUTOMSG_E102_E_HD/aprf_04_e.png
+xcopy TUTOMSG_SONIC_E_HD\sprf_01_e.png TUTOMSG_TAILS_E_HD\sprf_01_e.png /Y
+xcopy TUTOMSG_SONIC_E_HD\sprf_02_e.png TUTOMSG_TAILS_E_HD\sprf_02_e.png /Y
+xcopy TUTOMSG_SONIC_E_HD\sprf_04_e.png TUTOMSG_TAILS_E_HD\sprf_04_e.png /Y
+xcopy TUTOMSG_TAILS_E_HD\tprf_06_e.png TUTOMSG_KNUCKLES_E_HD\tprf_06_e.png /Y
+xcopy TUTOMSG_AMY_E_HD\aprf_04_e.png TUTOMSG_BIG_E_HD\aprf_04_e.png /Y
+xcopy TUTOMSG_AMY_E_HD\aprf_04_e.png TUTOMSG_E102_E_HD\aprf_04_e.png /Y
